35bb0f6865d4e41069b58b0ab11c01eefc6725a8
Aiman Mohd Said <37135470+aimanacho@users.noreply.github.com>
2020-11-03 23:37:38 +0800

    Add files via upload
    upload in cloud

---
+++ aboutme copy/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <link rel="stylesheet" href= "styleindex.css"/>
+    <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <!-- top bar -->
+    <div class="top-bar">
+      <ul class="nav justify-content-center">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About Me</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Education</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Experience</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Family</a>
+        </li>
+      </ul>
+    </div>
+
+
+    <!-- content -->
+    <div class="main">
+      <img class ="imageaiman" src="aiman.jpg">
+      <div class="aboutme">
+        <p><b>About Me</b></p>
+        <p style="font-size:14px;color:grey;"> TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST .</p>
+        <table class="table table-borderless">
+          <tbody>
+            <tr>
+              <td><b>Name:</b></td>
+              <td style="color:grey;">Aiman Naim Bin Mohd Said</td>
+            </tr>
+            <tr>
+              <td><b>Date of birth:</b></td>
+              <td style="color:grey;">12 January 2000</td>
+            </tr>
+            <tr>
+              <td><b>Address:</b></td>
+              <td style="color:grey;">Wangsa Maju, Seksyen 5</td>
+            </tr>
+            <tr>
+              <td><b>Zip Code:</b></td>
+              <td style="color:grey;">53300</td>
+            </tr>
+            <tr>
+              <td><b>Email:</b></td>
+              <td style="color:grey;">aimanacho@hotmail.com</td>
+            </tr>
+            <tr>
+              <td><b>Phone:</b></td>
+              <td style="color:grey;">017-3621718</td>
+            </tr>
+          </tbody>
+        </table>
+        <p style="color:blue;font-size:16px;"><b>5<span style="color:black;"> Project complete </span></b></p>
+        <form action="Resume.pdf" target="_blank">
+            <input type="submit" class="btn btn-primary" value="REVIEW CV" </input>
+        </form>
+      </div>
+    </div>
+  </body>
+</html>
